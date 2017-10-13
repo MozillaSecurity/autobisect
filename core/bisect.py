05fbffee5197afde901ede103b351762f048b342
pyoor <pyoor@users.noreply.github.com>
2017-10-13 10:25:53 -0400

    Major refactoring of core bisection routine

--- core/bisect.py
+++ core/bisect.py
@@ -6,24 +6,19 @@
 
 from __future__ import absolute_import, division, print_function
 
-import datetime
 import logging
+import os
+import shutil
 import subprocess
 import time
-import shutil
+from datetime import datetime, timedelta
 
-try:
-    # subprocess v3.5
-    from subprocess import DEVNULL
-except ImportError:
-    import os
-    DEVNULL = open(os.devnull, 'wb')
+import fuzzfetch
 
-from util import hgCmds
 from core.builds import BuildRange
-from core.builds import Build
-from core.boundary import Boundary
+from util import hgCmds
 
+DEVNULL = open(os.devnull, 'wb')
 log = logging.getLogger('bisect')
 
 
@@ -31,205 +26,229 @@ class Bisector(object):
     def __init__(self, args):
         self.target = args.target
         self.repo_dir = args.repo_dir
+        self.branch = hgCmds.get_branch_name(self.repo_dir)
         self.build_dir = args.build_dir
-        self.start = Boundary.new(args.start, self.repo_dir)
-        self.end = Boundary.new(args.end, self.repo_dir)
-        self.skip_revs = args.skip
+        self.build_flags = fuzzfetch.BuildFlags(asan=args.asan, debug=args.debug, fuzzing=False, coverage=False)
+
+        self.find_fix = args.find_fix
+        self.needs_verified = args.verify
+        self.mach_reduce = args.mach_reduce
+
+        self.start = fuzzfetch.Fetcher(self.target, self.branch, args.start, self.build_flags)
+        self.end = fuzzfetch.Fetcher(self.target, self.branch, args.end, self.build_flags)
+
         self.hg_prefix = ['hg', '-R', self.repo_dir]
-        self.asan = args.asan
-        self.debug = args.debug
 
         self.evaluator = None
         self.build_range = None
 
     def bisect(self):
         """
-        Bisect using mercurial repository
+        Main bisection function
         """
         log.info('Begin bisection...')
-        log.info('> Start: %s (%s)' % (self.start.rev, self.start.date))
-        log.info('> End: %s (%s)' % (self.end.rev, self.end.date))
+        log.info('> Start: %s (%s)' % (self.start.changeset, self.start.build_id))
+        log.info('> End: %s (%s)' % (self.end.changeset, self.end.build_id))
 
-        log.info('Purging all local repository changes')
-        subprocess.check_call(self.hg_prefix + ['update', '-C', 'default'], stdout=DEVNULL)
-        subprocess.check_call(self.hg_prefix + ['purge', '--all'], stdout=DEVNULL)
-
-        if not self.verify_bounds():
-            log.critical('Unable to validate supplied revisions.  Cannot bisect!')
+        if self.needs_verified and not self.verify_bounds():
+            log.critical('Unable to validate boundaries.  Cannot bisect!')
             return
 
+        log.info('Attempting to reduce bisection range using taskcluster binaries')
         if self.reduce_range():
-            log.info('Reduced build range to %s - %s' % (self.start.date, self.end.date))
+            log.info('Reduced build range to:')
+            log.info('> Start: %s (%s)' % (self.start.changeset, self.start.build_id))
+            log.info('> End: %s (%s)' % (self.end.changeset, self.end.build_id))
+        else:
+            log.warn('Unable to reduce bisection range using taskcluster binaries!')
 
-        log.info('Continue bisection using mercurial...')
-        # Reset bisect ranges and set skip ranges.
-        subprocess.check_call(self.hg_prefix + ['bisect', '-r'], stdout=DEVNULL)
-        if self.skip_revs:
-            log.info('Skipping revisions matching: {0}'.format(self.skip_revs))
-            for rev in self.skip_revs:
-                subprocess.check_call(self.hg_prefix + ['bisect', '--skip', rev])
+        if self.mach_reduce:
+            log.info('Continue bisection using mercurial...')
+            log.info('Purging all local repository changes')
+            subprocess.check_call(self.hg_prefix + ['purge', '--all'], stdout=DEVNULL)
+            subprocess.check_call(self.hg_prefix + ['pull'], stdout=DEVNULL)
+            subprocess.check_call(self.hg_prefix + ['update', '-C', 'default'], stdout=DEVNULL)
 
-        # Set bisection's start and end revisions
-        subprocess.check_call(self.hg_prefix + ['bisect', '-g', self.start.rev])
-        bisect_msg = subprocess.check_output(self.hg_prefix + ['bisect', '-b', self.end.rev])
-        current = Boundary.new(
-            hgCmds.get_bisect_changeset(
-                bisect_msg.splitlines()[0]
-            ),
-            self.repo_dir
-        )
-
-        iter_count = 0
-        skip_count = 0
-
-        while current is not None:
-            iter_count += 1
-            log.info('Begin round {0}: {1} ({2})'.format(iter_count, current.rev, current.date))
-            start_time = time.time()
-
+            # Clobber build directory once at start, then rely on AUTOCLOBBER
             self.clobber_build()
-            result = self.test_revision(current.rev)
 
-            if result == 'skip':
-                skip_count += 1
-                # If we use 'skip', we tell hg bisect to do a linear search to get around the skipping.
-                # If the range is large, doing a bisect to find the start and endpoints of compilation
-                # bustage would be faster. 20 total skips being roughly the time that the pair of
-                # bisections would take.
-                if skip_count > 20:
-                    log.error('Reached maximum skip attempts! Exiting')
-                    break
+            # Set bisection's start and end revisions
+            # Start and end revisions are flipped depending on self.find_fix
+            subprocess.check_call(self.hg_prefix + ['bisect', '-r'], stdout=DEVNULL)
+            good, bad = (self.end, self.start) if self.find_fix else (self.start, self.end)
+            subprocess.check_call(self.hg_prefix + ['bisect', '-g', good.changeset])
+            bisect_msg = subprocess.check_output(self.hg_prefix + ['bisect', '-b', bad.changeset])
+            current = hgCmds.get_full_hash(self.repo_dir, hgCmds.get_bisect_changeset(bisect_msg.splitlines()[0]))
 
-            current = self.update_hg(result, current)
+            iter_count = 0
+            skip_count = 0
 
-            end_time = time.time()
-            elapsed = datetime.timedelta(seconds=(int(end_time-start_time)))
-            log.info('Round {0} completed in {1}'.format(iter_count, elapsed))
-            hgCmds.destroy_pyc(self.repo_dir)
+            while current is not None:
+                iter_count += 1
+                log.info('Begin round %d - %s' % (iter_count, current))
+                start_time = time.time()
+
+                try:
+                    log.info('Attempting to find build for revision %s' % current)
+                    target_build = fuzzfetch.Fetcher(self.target, self.branch, current, self.build_flags)
+                except fuzzfetch.FetcherException:
+                    log.info('Unable to find build for revision %s' % current)
+                    # self.clobber_build()
+                    subprocess.check_call(self.hg_prefix + ['update', '-r', current], stdout=DEVNULL)
+                    result = self.evaluator.test_compilation()
+                else:
+                    result = self.test_build(target_build)
+
+                if result == 'skip':
+                    skip_count += 1
+                    # If we use 'skip', we tell hg bisect to do a linear search to get around the skipping.
+                    # If the range is large, doing a bisect to find the start and endpoints of compilation
+                    # bustage would be faster. 20 total skips being roughly the time that the pair of
+                    # bisections would take.
+                    if skip_count > 20:
+                        log.error('Reached maximum skip attempts! Exiting')
+                        summary = subprocess.check_output(
+                            self.hg_prefix + ['log', '-r', '"bisect(good) or bisect(bad)"', '--template',
+                                              '"Changeset: {rev}, Revision: {node|short} - {bisect}'])
+                        log.info('Summary of bisection:\n%s', summary)
+                        break
+
+                current = self.update_hg(result, current)
+
+                end_time = time.time()
+                elapsed = timedelta(seconds=(int(end_time-start_time)))
+                log.info('Round {0} completed in {1}'.format(iter_count, elapsed))
+                hgCmds.destroy_pyc(self.repo_dir)
 
     def reduce_range(self):
         """
         Reduce bisection range using taskcluster builds
+        :return: Boolean to identify whether the build range was successfully reduced
         """
-        log.info('Attempting to reduce bisection range using taskcluster builds')
-        # The oldest available build is 365 days from today
-        oldest_build_date = datetime.datetime.now().date() - datetime.timedelta(days=365)
+        orig_start = datetime.strptime(self.start.build_id, '%Y%m%d%H%M%S')
+        orig_end = datetime.strptime(self.end.build_id, '%Y%m%d%H%M%S')
 
-        # Taskcluster only retains builds up to a year old
-        # If self.end.date is older than a year, continue to compiled builds
-        if self.end.date < oldest_build_date:
-            log.error('End revision is more than a year old!')
-            log.error('Unable to reduce range using prebuilt binaries')
-            return False
-        if self.start.date < oldest_build_date:
-            log.warn('Start revision is more than a year old! Reducing with a start of %s' % oldest_build_date)
+        # Verify_bounds covers start and end so adjust by 1
+        build_range = BuildRange.new(orig_start + timedelta(days=1), orig_end - timedelta(days=1))
+        while len(build_range) != 0:
+            next_date = build_range.mid_point
+            i = build_range.index(next_date)
 
-        # If the start date is newer than the oldest available build, begin with that
-        if self.start.date > oldest_build_date:
-            oldest_build_date = self.start.date
-
-        builds = []
-        log.info('Identifying available builds between %s and %s' % (oldest_build_date, self.end.date))
-        for i in range((self.end.date - oldest_build_date).days + 1):
-            target_date = oldest_build_date + datetime.timedelta(days=i)
             try:
-                builds.append(Build(self.target, 'central', target_date, self.asan, self.debug))
-            except:
-                log.debug("Unable to retrieve build for date: %s" % str(target_date))
+                next_build = self.get_build(next_date)
+            except fuzzfetch.FetcherException:
+                next_build = None
 
-        self.build_range = BuildRange(builds)
+            if not next_build:
+                build_range.builds.pop(i)
+                continue
+            else:
+                log.info('Testing build %s' % next_build.build_id)
+                status = self.test_build(next_build)
 
-        while len(self.build_range) != 0:
-            self.clobber_build()
-            next_build = self.build_range.mid_point
-            log.info('Testing build from %s' % next_build.date)
-            next_build.build_info.extract_build(self.build_dir)
-            self.update_build_range(next_build.date)
+                if status == 'good':
+                    if not self.find_fix:
+                        self.start = next_build
+                        build_range = build_range[i+1:]
+                    else:
+                        self.end = next_build
+                        build_range = build_range[:i]
+                elif status == 'bad':
+                    if not self.find_fix:
+                        self.end = next_build
+                        build_range = build_range[:i]
+                    else:
+                        self.start = next_build
+                        build_range = build_range[i+1:]
+                elif status == "skip":
+                    build_range.builds.pop(i)
+                else:
+                    log.warn('Build range appears to be out of sync')
+                    log.warn('Removing build and continuing')
+                    build_range.builds.pop(i)
 
-        return True
+        # Was reduction successful?
+        if orig_start < datetime.strptime(self.start.build_id, '%Y%m%d%H%M%S'):
+            return True
+        if orig_end > datetime.strptime(self.end.build_id, '%Y%m%d%H%M%S'):
+            return True
+
+        return False
 
     def clobber_build(self):
         """
-        Delete and recreate build_dir
+        Delete the build directory and recreate it
         """
         if os.path.exists(self.build_dir):
             log.debug('Clobbering build dir:  %s' % self.build_dir)
             shutil.rmtree(self.build_dir)
             os.makedirs(self.build_dir)
 
-    def update_build_range(self, date):
+    def test_build(self, build):
         """
-        Update the build_range based on the results returned by the evaluator
-        Also update the start/end boundaries if later/newer than the previous value
+        Prepare the build directory and launch the supplied build
+        :param build: The build to test
+        :type: fuzzfetch.Fetcher
+        :return: The result of the build evaluation
         """
-        i = self.build_range.get_index(date)
-        status = self.evaluator.test_build()
-        revision = hgCmds.get_full_hash(
-            self.repo_dir,
-            self.build_range.builds[i].build_info.changeset
-        )
-
-        if status == "good" and date >= self.start.date:
-            self.start = Boundary(date, revision)
-            self.build_range = self.build_range[i+1:]
-        elif status == "bad" and date <= self.end.date:
-            self.end = Boundary(date, revision)
-            self.build_range = self.build_range[:i]
-        elif status == "skip":
-            self.build_range.builds.pop(i)
-        else:
-            log.warn('Build range appears to be out of sync')
-            log.warn('Removing build and continuing')
-            self.build_range.builds.pop(i)
+        self.clobber_build()
+        build.extract_build(self.build_dir)
+        return self.evaluator.evaluate_testcase()
 
     def verify_bounds(self):
         """
-        If start or end dates weren't adjusted via build reduction, test again
+        Verify that the supplied bounds behave as expected
+        :return: Boolean
         """
-        # Test that end revision crashes
-        self.clobber_build()
-        if self.test_revision(self.start.rev) != 'good':
+        log.info('Attempting to verify boundaries...')
+        status = self.test_build(self.start)
+        if status == 'skip':
+            log.critical('Unable to launch the start build!')
+        elif status == 'bad' and not self.find_fix:
             log.critical('Start revision crashes!')
             return False
 
-        self.clobber_build()
-        if self.test_revision(self.end.rev) != 'bad':
-            log.critical('End revision doesn\'t crash!')
+        status = self.test_build(self.end)
+        if status == 'skip':
+            log.critical('Unable to launch the end build!')
+        elif status == 'good' and not self.find_fix:
+            log.critical('End revision does not crash!')
             return False
 
+        log.info('Verified supplied boundaries!')
         return True
 
     def update_hg(self, label, current):
         """
-        Tell hg what we learned about the revision.
+        Informs mercurial of bisection result
+        :param label: The result of the bisection test
+        :param current: The revision to be marked
+        :return: The next revision or None
         """
         assert label in ('good', 'bad', 'skip')
-        log.info('Marking revision {0} as {1}'.format(current.rev, label))
-        results = subprocess.check_output(self.hg_prefix + ['bisect', '--' + label, current.rev])
+        log.info('Marking revision {0} as {1}'.format(current, label))
+        results = subprocess.check_output(self.hg_prefix + ['bisect', '--' + label, current])
 
         # Determine if we should continue
         # e.g. "Testing changeset 52121:573c5fa45cc4 (440 changesets remaining, ~8 tests)"
         bisect_msg = results.splitlines()[0]
         revision = hgCmds.get_bisect_changeset(bisect_msg)
         if revision:
-            log.info(bisect_msg)
-            return Boundary.new(revision, self.repo_dir)
+            return hgCmds.get_full_hash(self.repo_dir, revision)
 
         # Otherwise, return results
         log.info(results)
         return None
 
-    def test_revision(self, rev):
+    def get_build(self, build_string):
+        """
+        Retrieves a build for a specified value
+        :param build_string: String in the format of %Y-%m-%d or revision (SHA1) 
+        :return: A Fetcher build object
+        """
         try:
-            log.info('Attempting to find build for revision %s' % rev)
-            target_build = Build(self.target, 'central', rev, self.asan, self.debug)
-            target_build.build_info.extract_build(self.build_dir)
-            result = self.evaluator.test_build()
-            return result
-        except:
-            log.warn('Unable to find build.  Falling back to compilation')
-
-        subprocess.check_call(self.hg_prefix + ['update', '-r', rev], stdout=DEVNULL)
-        result = self.evaluator.test_compilation()
-
-        return result
+            build = fuzzfetch.Fetcher(self.target, self.branch, build_string, self.build_flags)
+            log.info('Found build: %s (%s)' % (build.changeset, build.build_id))
+            return build
+        except fuzzfetch.FetcherException:
+            log.warn('Unable to retrieve build for %s' % build_string)

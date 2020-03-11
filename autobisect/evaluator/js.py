203f5e34ed5933c7340661821b14a9d11448b498
pyoor <pyoor@users.noreply.github.com>
2020-03-11 14:29:12 -0400

    Check that supplied flags are valid for current build

--- autobisect/evaluator/js.py
+++ autobisect/evaluator/js.py
@@ -3,12 +3,53 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import logging
 import os
+from string import Template
 
+import requests
 from lithium import interestingness
 
 from ..bisect import Bisector
 
 log = logging.getLogger("js-eval")
+
+HTTP_SESSION = requests.Session()
+
+FLAGS_URL = Template(
+    "https://hg.mozilla.org/mozilla-unified/raw-file/$rev/js/src/shell/fuzz-flags.txt"
+)
+
+
+def _get_rev(binary):
+    """
+    Return either the revision specified in the fuzzmanagerconf
+    :param binary: Path to build
+    """
+    path = f"{os.path.splitext(binary)[0]}.fuzzmanagerconf"
+    if os.path.isfile(path):
+        with open(path) as file:
+            for line in file.readlines():
+                if line.startswith("product_version"):
+                    return line.split("-")[1].strip()
+
+
+def get_valid_flags(rev):
+    """
+    Extract list of runtime flags available to the current build
+    :param rev:
+    """
+    flags = []
+
+    try:
+        data = HTTP_SESSION.get(FLAGS_URL.substitute(rev=rev), stream=True)
+        data.raise_for_status()
+        for line in data.text.split("\n"):
+            flag = line.strip()
+            if flag and not flag.startswith("#"):
+                flags.append(flag)
+    except requests.exceptions.RequestException as e:
+        log.warn("Failed to retrieve build flags", e)
+
+    return flags
 
 
 class JSEvaluatorException(Exception):
@@ -48,16 +89,17 @@ class JSEvaluator(object):
             self._match = kwargs.get("match")
             self._regex = kwargs.get("regex")
 
-    def verify_build(self, binary):
+    def verify_build(self, binary, flags=None):
         """
         Verify that build doesn't crash on start
         :param binary: The path to the target binary
+        :param flags: Runtime flags
         :return: Boolean
         """
         log.info("> Verifying build...")
         args = [binary, "-e", '"quit()"']
-        if self.flags is not None:
-            args.extend(self.flags)
+        if flags is not None:
+            args.extend(flags)
         run_data = interestingness.timed_run.timed_run(args, self.timeout, None)
         if run_data.sta is not interestingness.timed_run.NORMAL:
             log.error(">> Build crashed!")
@@ -72,13 +114,21 @@ class JSEvaluator(object):
         """
         binary = os.path.join(build_path, "dist", "bin", "js")
         common_args = ["-t", "%s" % self.timeout, binary]
+
+        flags = self.flags
         if self.flags is not None:
-            common_args.extend(self.flags)
+            rev = _get_rev(binary)
+            all_flags = get_valid_flags(rev)
+            if all_flags:
+                flags = [flag for flag in self.flags if flag in all_flags]
+
+        if flags is not None:
+            common_args.extend(flags)
 
         # These args are global to all detect types
         common_args.append(self.testcase)
 
-        if self.verify_build(binary):
+        if self.verify_build(binary, flags):
             for _ in range(self.repeat):
                 log.info("> Launching build with testcase...")
                 if self.detect == "diff":

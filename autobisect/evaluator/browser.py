ac837478430f7a1ba192d31c14ab9e9db28ad1f7
pyoor <pyoor@users.noreply.github.com>
2020-04-17 12:11:37 -0400

    Replace ffpuppet with grizzly.replay

--- autobisect/evaluator/browser.py
+++ autobisect/evaluator/browser.py
@@ -6,8 +6,11 @@ import os
 import tempfile
 from contextlib import contextmanager
 
-from ffpuppet import FFPuppet, LaunchError
+from grizzly.common import TestCase
+from grizzly.replay import ReplayManager
+from grizzly.target import TargetLaunchError, TargetLaunchTimeout, load as load_target
 from prefpicker import PrefPicker
+from sapphire import Sapphire
 
 from ..bisect import Bisector
 
@@ -19,25 +22,21 @@ class BrowserEvaluator(object):
     Testcase evaluator for Firefox
     """
 
-    # asserts=False, detect=None, gdb=False, valgrind=False, xvfb=True, timeout=60,
-    #                  launch_timeout=300, ext=None, prefs=None, memory=None
-
     def __init__(self, testcase, **kwargs):
         self.testcase = testcase
-        self.repeat = kwargs.get("repeat", 1)
 
         # FFPuppet arguments
-        self._asserts = kwargs.get("asserts", False)
-        self._detect = kwargs.get("detect", "crash")
-        self._use_gdb = kwargs.get("gdb", False)
+        self._repeat = kwargs.get("repeat", 1)
+        self._ignore = kwargs.get("ignore", ["log-limit", "memory", "timeout"])
         self._use_valgrind = kwargs.get("valgrind", False)
         self._use_xvfb = kwargs.get("xvfb", True)
         self._timeout = kwargs.get("timeout", 60)
         self._launch_timeout = kwargs.get("launch_timeout", 300)
-        self._extension = kwargs.get("ext", None)
         self._prefs = kwargs.get("prefs", None)
-        self._memory = kwargs.get("memory", 0) * 1024 * 1024
         self._env_vars = kwargs.get("env", None)
+
+        if logging.getLogger().level != logging.DEBUG:
+            logging.getLogger("grizzly").setLevel(logging.WARNING)
 
     @contextmanager
     def prefs(self):
@@ -65,9 +64,10 @@ class BrowserEvaluator(object):
         """
         Verify that build doesn't crash on start
         :param binary: The path to the target binary
+        :param prefs: The path to the prefs file
         :return: Boolean
         """
-        with tempfile.NamedTemporaryFile() as temp:
+        with tempfile.NamedTemporaryFile(suffix=".html") as temp:
             with open(temp.name, "w") as f:
                 f.write("<html><script>window.close()</script></html>")
 
@@ -78,6 +78,7 @@ class BrowserEvaluator(object):
             log.error(">> Build crashed!")
             return False
 
+        log.error(">> Build verified!")
         return True
 
     def evaluate_testcase(self, build_path):
@@ -89,80 +90,64 @@ class BrowserEvaluator(object):
         result = Bisector.BUILD_FAILED
         with self.prefs() as prefs_file:
             if os.path.isfile(binary) and self.verify_build(binary, prefs=prefs_file):
-                for _ in range(self.repeat):
-                    log.info("> Launching build with testcase...")
-                    result = self.launch(binary, self.testcase, prefs_file)
-                    if result == Bisector.BUILD_CRASHED:
-                        break
+                log.info("> Launching build with testcase...")
+                result = self.launch(binary, self.testcase, prefs_file)
 
         return result
 
-    def launch(self, binary, testcase, prefs=None):
+    def launch(self, binary, test_path, prefs=None):
         """
         Launch firefox using the supplied binary and testcase
         :param binary: The path to the firefox binary
-        :param testcase: The path to the testcase
+        :param test_path: The path to the testcase
         :param prefs: The path to the prefs file
         :return: The return code or None
         """
-        ffp = FFPuppet(
-            use_gdb=self._use_gdb,
-            use_valgrind=self._use_valgrind,
-            use_xvfb=self._use_xvfb,
-        )
-        if self._asserts:
-            ffp.add_abort_token("###!!! ASSERTION:")
-
-        result = Bisector.BUILD_PASSED
+        replay = None
+        target = None
+        testcase = TestCase.load_path(test_path)
+        if self._env_vars:
+            for key, value in self._env_vars:
+                testcase.add_environ_var(key, value)
 
         try:
-            ffp.launch(
-                str(binary),
-                env_mod=self._env_vars,
-                location=testcase,
+            target = load_target("ffpuppet")(
+                binary,
+                extension=None,
+                log_limit=None,
+                memory_limit=None,
+                relaunch=1,
                 launch_timeout=self._launch_timeout,
-                memory_limit=self._memory,
-                prefs_js=prefs,
-                extension=self._extension,
+                prefs=prefs,
+                valgrind=self._use_valgrind,
+                xvfb=self._use_xvfb,
             )
-            ffp.wait(self._timeout)
 
-            if not ffp.is_running():
-                ffp.close()
-                if ffp.reason == FFPuppet.RC_EXITED:
-                    log.info(">> Target closed itself")
-                elif (
-                    ffp.reason == FFPuppet.RC_WORKER
-                    and self._detect == "memory"
-                    and "ffp_worker_memory_limiter" in ffp.available_logs()
-                ):
-                    log.info(">> Memory limit exceeded")
-                    result = Bisector.BUILD_CRASHED
-                elif (
-                    ffp.reason == FFPuppet.RC_WORKER
-                    and self._detect == "log"
-                    and "ffp_worker_log_size_limiter" in ffp.available_logs()
-                ):
-                    log.info(">> Log size limit exceeded")
-                    result = Bisector.BUILD_CRASHED
-                else:
-                    log.info(">> Failure detected")
-                    result = Bisector.BUILD_CRASHED
-            elif not ffp.is_healthy():
-                # this should be e10s only
-                result = Bisector.BUILD_CRASHED
-                log.info(">> Browser is alive but has crash reports. Terminating...")
-                ffp.close()
-            elif self._detect == "timeout":
-                result = Bisector.BUILD_CRASHED
-                log.info(">> Timeout detected")
-                ffp.close()
+            with Sapphire(auto_close=1, timeout=self._timeout) as server:
+                target.reverse(server.port, server.port)
+                replay = ReplayManager(
+                    self._ignore,
+                    server,
+                    target,
+                    testcase,
+                    any_crash=False,
+                    signature=None,
+                    use_harness=False,
+                )
+                success = replay.run(repeat=self._repeat)
+
+            if success:
+                return Bisector.BUILD_CRASHED
             else:
-                log.info(">> Time limit exceeded")
-        except LaunchError:
-            log.warn(">> Failed to start browser")
-            result = Bisector.BUILD_FAILED
-        finally:
-            ffp.clean_up()
+                return Bisector.BUILD_PASSED
 
-        return result
+        except (TargetLaunchError, TargetLaunchTimeout):
+            return Bisector.BUILD_FAILED
+
+        finally:
+            if replay is not None:
+                replay.cleanup()
+            if target is not None:
+                target.cleanup()
+            if testcase is not None:
+                testcase.cleanup()

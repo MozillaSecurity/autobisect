71e55d62fee529c5d6f557af02b4b094dd93386d
pyoor <pyoor@users.noreply.github.com>
2020-05-08 10:28:41 -0400

    Abstract class must inherit from ABC

--- autobisect/evaluators/base.py
+++ autobisect/evaluators/base.py
@@ -1,7 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from enum import Enum
 
 
@@ -15,7 +15,7 @@ class EvaluatorResult(Enum):
     BUILD_FAILED = 2
 
 
-class Evaluator(object):
+class Evaluator(ABC):
     """
     Base evaluator class
     """

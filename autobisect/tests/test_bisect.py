5e147fb25af0c9c9065da2b3b2d1128fb58fdb94
pyoor <pyoor@users.noreply.github.com>
2020-05-08 11:43:49 -0400

    Simplify tests

--- autobisect/tests/test_bisect.py
+++ autobisect/tests/test_bisect.py
@@ -1,104 +1,131 @@
-from contextlib import contextmanager
+import re
 from datetime import datetime, timedelta
 
 import pytest
 import requests_mock
 from freezegun import freeze_time
-from fuzzfetch import BuildFlags, Fetcher, Platform
+from fuzzfetch import BuildFlags, Platform
 
 from .fetcher_callback import fetcher_mock
 from .. import EvaluatorResult
 from ..bisect import Bisector, get_autoland_range
 from ..builds import BuildRange
 
-MOCK_DATE = "2020-01-01"
-
-TARGET = "firefox"
-BRANCH = "central"
-PLATFORM = Platform("Linux", "x86_64")
-FLAGS = BuildFlags(
-    asan=False, tsan=False, debug=False, fuzzing=False, coverage=False, valgrind=False,
-)
+# pylint: disable=protected-access
 
 
-@contextmanager
-def init_bisector(start=None, end=None):
+class MockFetcher:
     """
-    Wrapper for context managers needed to init Bisector
+    Class for mocking Fetcher objects
     """
-    with freeze_time("2020-01-01"), requests_mock.Mocker() as req:
-        # Mock all fetcher requests
-        req.register_uri(requests_mock.ANY, requests_mock.ANY, content=fetcher_mock)
-        yield Bisector(None, TARGET, BRANCH, start, end, FLAGS, PLATFORM)
+
+    def __init__(self, dt=None, changeset=None):
+        self.datetime = dt
+        self.changeset = changeset
+
+
+class MockBisector(Bisector):
+    """
+    Class for mocking Bisector objects
+    """
+
+    # pylint: disable=super-init-not-called
+    def __init__(self, start, end):
+        self.start = MockFetcher(dt=start)
+        self.end = MockFetcher(dt=end)
+        self.target = "firefox"
+        self.branch = "central"
+        self.flags = BuildFlags(
+            asan=False,
+            tsan=False,
+            debug=False,
+            fuzzing=False,
+            coverage=False,
+            valgrind=False,
+        )
+        self.platform = Platform("Linux", "x86_64")
 
 
 def test_bisect_get_daily_builds_simple():
     """
     Simple test for Bisector._get_daily_builds
     """
-    with init_bisector() as bisector:
-        # pylint: disable=protected-access
-        builds = bisector._get_daily_builds()
-        assert isinstance(builds, BuildRange)  # Returns BuildRange
-        assert all(isinstance(b, str) for b in builds)
-        assert len(builds) == 364  # Length 365 - 1
+    start_date = datetime.now() - timedelta(days=10 + 1)
+    end_date = datetime.now()
+
+    bisector = MockBisector(start_date, end_date)
+    builds = bisector._get_daily_builds()
+
+    assert isinstance(builds, BuildRange)  # Returns BuildRange
+    assert all(re.match(r"\d{4}-\d{2}-\d{2}", b) is not None for b in builds)
+    assert len(builds) == 10  # Length 365 - 1
 
 
 def test_bisect_get_daily_builds_no_builds():
     """
     Test Bisector._get_daily_builds using an empty build range
     """
-    with init_bisector(MOCK_DATE, MOCK_DATE) as bisector:
-        # pylint: disable=protected-access
-        builds = bisector._get_daily_builds()
-        assert isinstance(builds, BuildRange)  # Returns BuildRange
-        assert len(builds) == 0
+    start_date = datetime.now()
+    bisector = MockBisector(start_date, start_date)
+    builds = bisector._get_daily_builds()
+
+    assert isinstance(builds, BuildRange)  # Returns BuildRange
+    assert len(builds) == 0  # Length 365 - 1
 
 
-def test_bisect_get_pushdate_builds_simple():
+def test_bisect_get_pushdate_builds_simple(mocker):
     """
     Simple test of Bisector._get_pushdate_builds
     """
-    end = datetime.strptime(MOCK_DATE, "%Y-%m-%d")
-    start = datetime.strftime(end - timedelta(days=1), "%Y-%m-%d")
-    with init_bisector(start, MOCK_DATE) as bisector:
-        # pylint: disable=protected-access
-        builds = bisector._get_pushdate_builds()
-        assert isinstance(builds, BuildRange)  # Returns BuildRange
-        assert len(builds) > 1
-        for build in builds:
-            assert isinstance(build, Fetcher)
-            assert datetime.strftime(build.datetime, "%Y-%m-%d") == start
+    end_date = datetime(2020, 1, 1, 0, 0)
+    start_date = end_date - timedelta(days=1)
+    bisector = MockBisector(start_date, end_date)
+
+    # Generate fake pushdates
+    pushdates = []
+    for dt in [start_date, end_date]:
+        group = []
+        end_of_day = dt.replace(hour=23, minute=59, second=59)
+        delta = (end_of_day - dt) / 10
+        current = dt
+        while current < end_of_day:
+            group.append(MockFetcher(dt=current))
+            current = current + delta
+        pushdates.append(group)
+
+    mocker.patch("autobisect.bisect.Fetcher.iterall", side_effect=pushdates)
+    builds = bisector._get_pushdate_builds()
+
+    assert isinstance(builds, BuildRange)
+    assert len(builds) == 9
 
 
 def test_bisect_get_autoland_builds_simple():
     """
     Simple test of Bisector._get_autoland_builds
     """
-    start = "03ed5ed6cba7190608c548578e9d79d82b0ccf83"
-    end = "a1266665b89b04eaa8c146d0bc91b512945423cb"
-    with init_bisector(start, end) as bisector:
-        # pylint: disable=protected-access
+    bisector = MockBisector(None, None)
+    bisector.start = MockFetcher(dt=datetime(2019, 12, 30), changeset="03ed5ed6cba7")
+    bisector.end = MockFetcher(dt=datetime(2019, 12, 31), changeset="a1266665b89b")
+
+    with requests_mock.Mocker() as req:
+        req.register_uri(requests_mock.ANY, requests_mock.ANY, content=fetcher_mock)
         builds = bisector._get_autoland_builds()
-        assert isinstance(builds, BuildRange)  # Returns BuildRange
+        repo_url = "https://hg.mozilla.org/integration/autoland"
+
+        assert isinstance(builds, BuildRange)
         assert len(builds) > 1
-        for build in builds:
-            assert (
-                build.build_info["moz_source_repo"]
-                == "https://hg.mozilla.org/integration/autoland"
-            )
+        assert all(b.build_info["moz_source_repo"] == repo_url for b in builds)
 
 
 def test_get_autoland_range_simple():
     """
     Simple test of get_autoland_range()
     """
-    with requests_mock.Mocker() as req:
-        # Mock all fetcher requests
+    with freeze_time("2020-01-01"), requests_mock.Mocker() as req:
         req.register_uri(requests_mock.ANY, requests_mock.ANY, content=fetcher_mock)
-        start = "03ed5ed6cba7190608c548578e9d79d82b0ccf83"
-        end = "a1266665b89b04eaa8c146d0bc91b512945423cb"
-        changesets = get_autoland_range(start, end)
+        changesets = get_autoland_range("03ed5ed6cba7", "a1266665b89b")
+
         assert len(changesets) == 28
         for changeset in changesets:
             assert isinstance(changeset, str) and len(changeset) == 40
@@ -109,7 +136,6 @@ def test_get_autoland_range_invalid_revs():
     Test get_autoland_range using invalid revisions
     """
     with requests_mock.Mocker() as req:
-        # Mock all fetcher requests
         req.register_uri(requests_mock.ANY, requests_mock.ANY, content=fetcher_mock)
         assert get_autoland_range("foo", "bar") is None
 
@@ -119,32 +145,25 @@ def test_get_autoland_range_multiple_revs():
     Test that get_autoland_range returns None when multiple changsets identified
     """
     with requests_mock.Mocker() as req:
-        # Mock all fetcher requests
         req.register_uri(requests_mock.ANY, requests_mock.ANY, content=fetcher_mock)
-        start = "385f49adaf00d02fc8e04da1e0031e3477182d67"
-        end = "590613078c743f089f01b3f43118524e9553dd0b"
-        assert get_autoland_range(start, end) is None
+        assert get_autoland_range("385f49adaf00", "590613078c74") is None
 
 
-STATUSES = [
-    EvaluatorResult.BUILD_PASSED,
-    EvaluatorResult.BUILD_CRASHED,
-    EvaluatorResult.BUILD_FAILED,
-]
-BUILDS = [1, 2, 3]
-FIX_OPTIONS = [True, False]
-
-
-@pytest.mark.parametrize("build", BUILDS)
-@pytest.mark.parametrize("status", STATUSES)
-@pytest.mark.parametrize("find_fix", FIX_OPTIONS)
-def test_update_range_simple(build, status, find_fix):
+@pytest.mark.parametrize("status", EvaluatorResult)
+@pytest.mark.parametrize("find_fix", [True, False])
+def test_update_range_simple(status, find_fix):
     """
     Simple test of Bisector.update_range
     """
-    with init_bisector() as bisector:
+    builds = []
+    for _ in range(10):
+        next_date = datetime(2020, 1, 1, 0, 0) + timedelta(days=1)
+        builds.append(MockFetcher(dt=next_date))
+
+    for build in builds:
+        bisector = MockBisector(builds[0], builds[-1])
         bisector.find_fix = find_fix
-        build_range = BuildRange(BUILDS)
+        build_range = BuildRange(builds)
         bisector.update_range(status, build, build_range)
         if status == EvaluatorResult.BUILD_PASSED:
             if find_fix:
